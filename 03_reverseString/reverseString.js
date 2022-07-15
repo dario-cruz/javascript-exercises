7c9539a7e268955737fad2352ce545a6122c91ac
Dario Cruz <rio2012@gmail.com>
2022-07-15 19:00:04 -0400

    Add all pass solution code 03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(textString) {
+    let textArray = textString.split("");
+    let textArrayRev = textArray.reverse();
+    let textReverse = textArrayRev.join("");
+    return textReverse;
 };
 
 // Do not edit below this line
