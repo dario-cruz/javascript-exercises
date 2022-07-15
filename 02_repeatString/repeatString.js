62516c1c93182052073a046eb3be072f3a38441b
Dario Cruz <rio2012@gmail.com>
2022-07-15 18:40:54 -0400

    Add all pass solution code, 02-repeatString.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        let errorString = 'ERROR';
+        return errorString;
+        
+    } else {
+        let processedString = string.repeat(num);
+        return processedString;
+    }
+    // Var for output string. 
 };
 
 // Do not edit below this line
