d29406155a9f34197e2814f618d69da74fe70488
Cruz <dariocru@amazon.com>
2022-07-18 13:41:04 -0400

    Add solution code, first test, 05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    const numsArray =[];
+    while (numOne <= numTwo) {
+        numsArray.push(numOne);
+        numOne++;
+    }
+    let result = numsArray.reduce((sum, current) => sum + current, 0);
+    return result;
 };
 
 // Do not edit below this line
