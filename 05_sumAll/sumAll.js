24553c4d98a40b9b9865c4cb042d8a02caee8bd1
Cruz <dariocru@amazon.com>
2022-07-18 13:55:57 -0400

    Add solution code for 05-sumAll passsed most tests. Does not pass neg nums. Does not pass non-num.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,26 @@
 const sumAll = function(numOne, numTwo) {
     const numsArray =[];
-    while (numOne <= numTwo) {
-        numsArray.push(numOne);
-        numOne++;
+    if (isNaN(numOne)) {
+        return "ERROR";
+    } else if (isNaN(numTwo)) {
+        return "ERROR";
+    } else {
+        if (numOne > numTwo) {
+            while (numOne >= numTwo) {
+                numsArray.push(numTwo);
+                numTwo++;
+            }
+            let result = numsArray.reduce((sum, current) => sum + current, 0);
+            return result;
+        } else {
+            while (numOne <= numTwo) {
+                numsArray.push(numOne);
+                numOne++;
+            }
+            let result = numsArray.reduce((sum, current) => sum + current, 0);
+            return result;
+        }
     }
-    let result = numsArray.reduce((sum, current) => sum + current, 0);
-    return result;
 };
 
 // Do not edit below this line
