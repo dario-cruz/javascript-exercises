ded60b21fc6dc11ca81c12e221492d496ea0dc2b
Cruz <dariocru@amazon.com>
2022-07-18 12:01:10 -0400

    Add solution code for 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...inputNum) {
+    result = inputArray.filter(items => !inputNum.includes(items));
+    return result;
 };
 
 // Do not edit below this line
