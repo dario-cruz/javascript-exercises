6ef9e702a74022caad8319086359629be66e91c1
Cruz <dariocru@amazon.com>
2022-07-18 12:04:17 -0400

    Add explaination comments for 04-removeFromArray. Learned a lot from this one.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,3 +1,10 @@
+//Function makes use of rest parameters. 
+//Rest parameters allow for indefinate ammounts of args in a functon.
+//Args in a rest param get stored as an array, so you can use array methods on them.
+
+//function takes the first arg which is an array and filters it with the rest param.
+//Filter points to an arrow function that filers all items not in the "inputNum" array.
+
 const removeFromArray = function(inputArray, ...inputNum) {
     result = inputArray.filter(items => !inputNum.includes(items));
     return result;
