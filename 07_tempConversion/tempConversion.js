bd6b059341d669c140f6cfa69f993a5a0012e186
Cruz <dariocru@amazon.com>
2022-07-19 09:01:10 -0400

    Add solution code 07-tempConv, passes the first test.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(inputTemp) {
+  result = (inputTemp - 32) * .5556;
+  Math.round(result);
+  return result;
 };
 
-const ctof = function() {
-
+const ctof = function(inputTemp) {
+  result = (inputTemp * 1.8) + 32;
+  Math.round(result);
+  return result;
 };
 
 // Do not edit below this line
