07f68bf1c232aa41fa4341a5565d5d58c5dafda8
Cruz <dariocru@amazon.com>
2022-07-19 08:44:10 -0400

    Add comments to solution code 06-leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,3 +1,5 @@
+// Function checkes the arg against modulo expressions and passes it on to the logic.
+// Then the output will be true or false.
 const leapYears = function(inputYears) {
     if (inputYears % 4 == 0 && inputYears % 100 !== 0 || inputYears % 400 == 0) {
         return true;
