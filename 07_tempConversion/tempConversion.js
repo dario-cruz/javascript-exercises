e6a3ee76a8573ee5c2f338c1f9309dd6fb0c1a20
Cruz <dariocru@amazon.com>
2022-07-19 09:08:48 -0400

    Add solution code 07-temp passed all tests. Add comments for explainaton

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
 const ftoc = function(inputTemp) {
+  //Calc for f to c conversion.
   result = (inputTemp - 32) * .5556;
-  Math.round(result);
-  return result;
+  //Math round method to the 1st decimal place. 
+  resultRoundc = Math.round(result * 10) / 10;
+  return resultRoundc;
 };
 
 const ctof = function(inputTemp) {
+  //Calc for c to f conversion.
   result = (inputTemp * 1.8) + 32;
-  Math.round(result);
-  return result;
+  //Math round method to the 1st decimal place. 
+  resultRoundf = Math.round(result * 10) / 10;
+  return resultRoundf;
 };
 
 // Do not edit below this line
