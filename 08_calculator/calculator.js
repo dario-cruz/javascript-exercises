d85bf9637643368a665cacf9226cc5bbd22141eb
Cruz <dariocru@amazon.com>
2022-08-19 11:51:34 -0400

    Add arraysum solution for 08-calc

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,20 @@ const subtract = function(a,b) {
 	return a - b;
 };
 
-const sum = function(a,b) {
-	
+const sum = function(numArray) {
+  // Working version with reduce method applied.
+  // Function defined for the reduce mehod to use on each array item.
+  const reducerFunc = (sum, val) => sum + val;
+  // Initial value to start with. 
+  const total = 0;
+  return numArray.reduce(reducerFunc, total);
+
+  // Working non-reduce method example.
+  // let total = 0;
+  // for (const num of numArray) {
+  //   total += num;
+  // }
+  // return total;
 };
 
 const multiply = function() {
