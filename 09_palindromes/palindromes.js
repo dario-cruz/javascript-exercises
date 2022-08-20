f119648a87ae7ac372f75931549abcbb200f69f1
Dario Cruz <rio2012@gmail.com>
2022-08-20 07:24:46 -0400

    Add partial solution code 09_palindromes.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let firstPass = string.replace('!','').toLocaleLowerCase();
+    let secondPass = firstPass.replace(',','').replace('.','').replace(' ','');
+    let modString = secondPass.split("").reverse().join("");
+    if (modString === secondPass) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
