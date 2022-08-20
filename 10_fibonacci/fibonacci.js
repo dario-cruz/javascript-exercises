310f89676db8b985a7ffc7e7f4f9c43f854945e0
Dario Cruz <rio2012@gmail.com>
2022-08-20 08:07:17 -0400

    Add formula for fib seq and initial funcition.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+phi = (Math.sqrt(5) + 1) / 2;
+phiminus = (Math.sqrt(5) - 1) / 2;
+const fibonacci = function(num) {
+    calRaw = ((phi ** num) - (phiminus ** num) / Math.sqrt(5));
+    calRes = Math.round(calRaw);
 };
 
 // Do not edit below this line
