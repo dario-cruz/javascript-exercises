4241998a9ff7568e88cd52aa917df7e7342fd4c0
Dario Cruz <rio2012@gmail.com>
2022-08-22 10:38:02 -0400

    Add solution code for 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    // Returns the array of titles from the objects.
+    return books.map(function(element) {
+        return element.title;
+    })
 };
 
 // Do not edit below this line
