dc18634157fb8c19558188229a81378c4969198d
Dario Cruz <rio2012@gmail.com>
2022-08-24 11:44:06 -0400

    Add partial solution code 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let ageArray = array.map((element) => {
+        // Map method itierates through the array with the given function.
+        // Function gets the age of the person and puts it into a resulting array.
+        return element.yearOfDeath - element.yearOfBirth;
+    })
+    // Making use of the Math.max method to find the index of the max age. 
+    let getIndex = Math.max(...ageArray);
+    // Defining the value to be used for selection.
+    let selectIndex = ageArray.getIndex;
+    // return the selection.
+    return array[selectIndex];
 };
 
 // Do not edit below this line
