936434245fbe5e861e4a73058758c519e0b25566
Dario Cruz <rio2012@gmail.com>
2022-08-25 10:25:28 -0400

    Add code for year info ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,17 +1,26 @@
+// Setting the current date. 
+// For tests where objects have no death date.
+const today = new Date();
+const getDate = today.getFullYear();
+
 const findTheOldest = function(people) {
-    let ageArray = people.map((element) => {
+    const ageArray = people.map((element) => {
         // Map method itierates through the array with the given function.
         // Function gets the age of the person and puts it into a resulting array.
         return element.yearOfDeath - element.yearOfBirth;
     })
+    console.log(ageArray);
     // Making use of the Math.max method to find the index of the max age. 
     let getIndex = Math.max(...ageArray);
+    console.log(getIndex);
     // Defining the value to be used for selection.
-    let selectIndex = ageArray.getIndex;
+    let selectIndex = ageArray.indexOf(getIndex);
+    console.log(selectIndex);
     // return the selection.
     return people[selectIndex];
 };
 
+// Test array for troubleshooting. 
 const testPeople = [
     {
         name: "Carly",
