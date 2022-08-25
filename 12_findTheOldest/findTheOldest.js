b31bdd832d46a1433cd80c39f13a4987dbd29f5b
Dario Cruz <rio2012@gmail.com>
2022-08-25 09:37:37 -0400

    Add array of objs for testing 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -12,5 +12,23 @@ const findTheOldest = function(people) {
     return people[selectIndex];
 };
 
+const testPeople = [
+    {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+    },
+    {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+    },
+    {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+    },
+]
+
 // Do not edit below this line
 module.exports = findTheOldest;
